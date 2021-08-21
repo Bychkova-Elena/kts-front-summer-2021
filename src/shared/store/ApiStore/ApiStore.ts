c75ff2286d40f0dff685b603ac10715f1aa72601
Elena Bychkova <lena.bychkova.200012@gmail.com>
2021-08-21 13:40:15 +0300

    structure of folders done

---
+++ src/shared/store/ApiStore/ApiStore.ts
@@ -0,0 +1,31 @@
+import { IApiStore, RequestParams, ApiResponse } from "./types";
+
+export default class ApiStore implements IApiStore {
+  //url GitHub API
+  readonly baseUrl: string =
+    "https://docs.github.com/en/rest/overview/endpoints-available-for-github-apps";
+
+  // Метод, с помощью которого делается запрос.
+  async request<SuccessT, ErrorT = any, ReqT = {}>(
+    params: RequestParams<ReqT>
+  ): Promise<ApiResponse<SuccessT, ErrorT>> {
+    const url: string = `${this.baseUrl}/${params.endpoint}`;
+    try {
+      const response = await fetch(url, params);
+
+      const data = await response.json();
+
+      return {
+        success: true,
+        data,
+        status: response.status
+      };
+    } catch (error) {
+      return {
+        success: false,
+        data: error,
+        status: error.status
+      };
+    }
+  }
+}
